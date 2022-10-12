6e25fcdb2cdd0966bccc42c96597ef9bf942e956
bsalin39 <bsalin39@stu.southtexascollege.edu>
2022-10-12 16:43:02 -0500

    init cleanup

--- src/router.js
+++ src/router.js
@@ -1,15 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
 import HomePage from '@/views/HomePage.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: HomePage },
-  { path: '/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
+  { path: '/question/category/:id', name: 'Question', component: () => import('@/views/QuestionPage.vue') },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
 })
-
 export default router
