18e418960ebc654be7ce7b146021a4be2f644522
bsalin39 <bsalin39@stu.southtexascollege.edu>
2022-10-12 19:09:28 -0500

    category call

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup></script>
-
 <template>
   <main>
     <div class="card">
       <RouterView />
     </div>
   </main>
+  <p class="score">Score: 100</p>
 </template>
 
 <style lang="postcss" scoped>
@@ -14,5 +14,8 @@ main {
   & .card {
     @apply flex h-5/6 min-h-fit w-11/12 flex-col items-center gap-16 overflow-auto rounded-lg bg-white p-8 shadow-lg;
   }
+}
+.score {
+  @apply absolute bottom-6 right-8 text-2xl font-bold uppercase text-slate-700;
 }
 </style>
